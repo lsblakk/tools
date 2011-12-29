43ec06cf77151f89f4322b746a205fa4ae749247
Lukas Blakk <lukasblakk@gmail.com>
2011-12-28 17:28:42 -0800

    adding sqlite-specific schema

---
+++ scripts/autoland/data/autoland_sqlite.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 5.1.57, for apple-darwin10.7.4 (i386)
+--
+-- Host: localhost    Database: autoland
+-- ------------------------------------------------------
+-- Server version	5.1.57
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `branches`
+--
+
+DROP TABLE IF EXISTS `branches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `branches` (
+  `id` int(11) NOT NULL,
+  `name` text,
+  `repo_url` text,
+  `threshold` int(11) DEFAULT NULL,
+  `status` text,
+  PRIMARY KEY (`id`)
+) ;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patch_sets`
+--
+
+DROP TABLE IF EXISTS `patch_sets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patch_sets` (
+  `id` int(11) NOT NULL,
+  `bug_id` int(11) DEFAULT NULL,
+  `patches` text,
+  `author` text,
+  `retries` int(11) DEFAULT NULL,
+  `revision` text,
+  `branch` text,
+  `try_run` int(11) DEFAULT NULL,
+  `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `push_time` timestamp NULL DEFAULT NULL,
+  `completion_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-08-03 10:25:23
